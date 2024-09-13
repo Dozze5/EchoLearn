6cddb5ad11ad722244999391cceab350beb71b69
Bhanu Pratap Singh <114097203+bhanublez@users.noreply.github.com>
2024-09-13 20:06:24 +0530

    Authentication
    Working, UI Enhancing Required

---
+++ Authentication/config.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYtJ5cIzTfHuDXfBH1DsPSc1FvnEs2-rA",
+
+  authDomain: "echolearn-1.firebaseapp.com",
+
+  projectId: "echolearn-1",
+
+  storageBucket: "echolearn-1.appspot.com",
+
+  messagingSenderId: "159188415453",
+
+  appId: "1:159188415453:web:d7d5c39ca4ad447b9abc34",
+
+  measurementId: "G-14KY8PWQR7",
+};
+
+export { firebaseConfig };
